d99ded3d87e06fb4d1f3be50a05c0e1d0fed6d4c
jeanjp <jean_0720@hotmail.com>
2022-02-09 02:15:06 -0500

    Cambio urls

--- src/main/java/com/gestionPedidos/GestionPedidosApplication.java
+++ src/main/java/com/gestionPedidos/GestionPedidosApplication.java
@@ -18,7 +18,7 @@ public class GestionPedidosApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500").allowedMethods("GET","POST","PUT","DELETE");
+				registry.addMapping("/**").allowedOrigins("https://projectpedidos.herokuapp.com").allowedMethods("GET","POST","PUT","DELETE");
 			}
 		};
 	}
